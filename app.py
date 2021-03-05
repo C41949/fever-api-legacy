8fa5410720fd6cde945f855711b8a6a581c1d459
kammradt <vinicius.kammradt1@gmail.com>
2021-03-05 00:46:15 -0300

    feat: :sparkles: adds worker to save temperature

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
+import time
+from threading import Thread
 
 from flask import Flask
 
 from database import db, db_name
+from temperature import TemperatureService
 from temperature.views import temperatures
 
 
@@ -15,6 +18,15 @@ def create_app():
     return app
 
 
+def setup_worker():
+    temp = TemperatureService()
+
+    with app.app_context():
+        while True:
+            temp.create()
+            time.sleep(1)
+
+
 def setup_db():
     if not os.path.isfile(f'../{db_name}'):
         with app.app_context():
@@ -24,4 +36,5 @@ def setup_db():
 if __name__ == '__main__':
     app = create_app()
     setup_db()
-    app.run(port=8001, debug=True)
+    Thread(target=app.run).start()
+    Thread(target=setup_worker).start()
