24d715372ad26e0ddbfb9b9ea18cbcef96b437ca
kammradt <vinicius.kammradt1@gmail.com>
2021-03-29 23:35:13 -0300

    feat 🗃adds PostgreSQL instead of sqlite

--- api/database.py
+++ api/database.py
@@ -1,4 +1,3 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db_name = "db.db"
 db = SQLAlchemy()
--- api/wsgi.py
+++ api/wsgi.py
@@ -1,25 +1,22 @@
-import os
-
 from flask import Flask
 
-from database import db, db_name
+from database import db
 from logs.views import logs
 from temperature.views import temperatures
 
 application = Flask(__name__)
-application.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_name}'
+application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@fever-database:5432/temperature'
 application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(application)
 
 application.register_blueprint(temperatures)
 application.register_blueprint(logs)
+
+db.init_app(application)
+
+with application.app_context():
+    db.create_all()
 
 
 @application.route("/")
 def index():
     return 'Hi!'
-
-
-if not os.path.isfile(f'../{db_name}'):
-    with application.app_context():
-        db.create_all()
